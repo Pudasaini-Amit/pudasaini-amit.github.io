14191c31b2c78f30e35b479ebd3c439ad07c956a
Pudasaini Amit <pudasaini.amit@outlook.com>
2024-03-05 19:11:04 +1100

    new commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h2>Portfolio</h2>
+        <h3>Amit Pudasaini</h3>
       </header>
     </div>
   );
